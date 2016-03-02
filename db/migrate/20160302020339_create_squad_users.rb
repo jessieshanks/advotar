89116309a5d7cb2e4583ac40dcddfd4a20654951
Jessie Shanks <jessie.shanks@gmail.com>
2016-03-02 14:18:41 -0600

    basic login and getting to ones squad working

--- db/migrate/20160302020339_create_squad_users.rb
+++ db/migrate/20160302020339_create_squad_users.rb
@@ -1,15 +1,11 @@
 class CreateSquadUsers < ActiveRecord::Migration
   def change
     create_table :squad_users do |t|
-      t.squad_id :integer
-      t.user_id :integer
+      t.integer :squad_id
+      t.integer :user_id
 
       t.timestamps null: false
     end
-  end
-
-  def change
-    remove_column :users, :squad_id
   end
 
 end
