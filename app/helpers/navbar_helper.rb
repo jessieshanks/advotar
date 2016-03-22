6826d52f4efaca09e086f77aceb0c01a3a42027a
Jessie Shanks <jessie.shanks@gmail.com>
2016-03-22 14:22:09 -0500

    fixed environment variables

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -1,25 +1,25 @@
 module NavbarHelper
 
 
-module NavigationHelper
+# module NavigationHelper
 
-  def squad_edit_link(html_options = {})
-    link_to posts_archive_path, default_navigation_options(html_options)
-  end
+#   def squad_edit_link(html_options = {})
+#     link_to posts_archive_path, default_navigation_options(html_options)
+#   end
 
-  def about_link(html_options = {})
-    link_to posts_archive_path, default_navigation_options(html_options)
-  end
+#   def about_link(html_options = {})
+#     link_to posts_archive_path, default_navigation_options(html_options)
+#   end
 
-  def works_link(html_options = {})
-    link_to posts_archive_path, default_navigation_options(html_options)
-  end
+#   def works_link(html_options = {})
+#     link_to posts_archive_path, default_navigation_options(html_options)
+#   end
 
-  private
+#   private
 
-  def default_navigation_options(html_options)
-    html_options[:class] << 'item'
-  end
+#   def default_navigation_options(html_options)
+#     html_options[:class] << 'item'
+#   end
 
 
 end
